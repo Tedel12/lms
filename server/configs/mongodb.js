18e06a6e8601543b0f386ea9ab8d326e42919542
ben ephraim <benagbannon@gmail.com>
2025-04-16 19:10:30 +0100

    Deuxième commit

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
    mongoose.connection.on('connected', () => console.log('Connected'))
-   await mongoose.connect('mongodb://localhost:27017/lms')
+   await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 };
 
 
--- server/controllers/webHooks.js
+++ server/controllers/webHooks.js
@@ -8,9 +8,9 @@ export const clerkWebhooks = async (req, res) =>{
         const whook =  new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         await whook.verify(JSON.stringify(req.body), {
-            "svix-id": req.header["svix-id"],
-            "svix-timestramp": req.header["svix-timestramp"],
-            "svix-signature": req.header["svix-signature"],
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"],
         })
         
         const {data, type} = req.body
@@ -21,7 +21,7 @@ export const clerkWebhooks = async (req, res) =>{
                     _id: data.id,
                     email: data.email_address[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.imageUrl,
+                    imageUrl: data.image_url,
                 }
                 await User.create(userData)
                 res.json({})
@@ -32,7 +32,7 @@ export const clerkWebhooks = async (req, res) =>{
                 const userData = {
                     email: data.email_address[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.imageUrl,
+                    imageUrl: data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id, userData)
                 res.json({})
