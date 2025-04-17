9f5a4965bca96cb7e4b02fa114a2cd57c459b6e9
ben ephraim <benagbannon@gmail.com>
2025-04-17 13:19:42 +0100

    Fix for Vercel

--- server/server.js
+++ server/server.js
@@ -17,9 +17,6 @@ app.use(cors())
 app.get('/', (req, res) => res.send('API working fine'))
 app.post('/clerk', express.json(), clerkWebhooks)
 
-//port
-const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+
+export default app
