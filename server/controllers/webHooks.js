b69912b8c7e5387f3892838cb8bff8ef697c7aca
ben ephraim <benagbannon@gmail.com>
2025-04-28 05:29:32 +0100

    modif du webhook

--- server/controllers/webHooks.js
+++ server/controllers/webHooks.js
@@ -8,15 +8,13 @@ export const clerkWebhooks = async (req, res) =>{
     try {
         const whook =  new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await whook.verify(req.body, {
+        await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"],
         })
         
-        const payload = JSON.parse(req.body);
-
-        const {data, type} = payload;
+        const {data, type} = req.body;
 
         console.log("webhook type:", type)
 
